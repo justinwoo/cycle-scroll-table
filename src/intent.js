f3fa92dd7fa555b53d007446e4dc3753a94587ab
Justin Woo <moomoowoo@gmail.com>
2015-07-06 14:19:33 +0900

    use my crappy log observables object

--- src/intent.js
+++ src/intent.js
@@ -1,9 +1,16 @@
+import logObservablesObject from 'log-observables-object';
+
 import {scrollTop$} from './intents/user-scroll';
 
 function intent() {
   let actions = {
     scrollTop$
   };
+
+  logObservablesObject(actions, {
+    debounceAllStreams: true
+  });
+
   return actions;
 }
 
